542e6f2f4c33cd9469e45bef5ec6d0f02a42845a
Jarrod Smith <jasmith2@fullsail.edu>
2016-04-14 21:56:46 -0500

    New file
    No code

--- All Things Weather/Resources/location.js
+++ All Things Weather/Resources/location.js
@@ -2,22 +2,42 @@
 
 var getGeo = function(lat,lng){
 	
-	if  (Ti.Geolocation.locationServicesEnabled){
-		Ti.Geolocation.purpose = ("Application needs your location cordinates");
-		Ti.Geolocation.getCurrentPosition(function(e){
-			console.log("raw data", e);
-		var lat = e.coords.latitude;
-		var lng = e.coords.longitude;
-        
+	if  (Ti.Platform.osname == "iphone"){
+	
+		if (Ti.Geolocation.locationServicesEnabled){
+		//Ti.Geolocation.purpose = ("Application needs your location cordinates");
+		Ti.Geolocation.getCurrentPosition = (function(e){
+		console.log(e);
+			var lat = e.coords.latitude;
+			console.log(lat);
+			var lng = e.coords.longitude;
+            
         console.log("theses are cordinates", lat, lng);
         var network = require("network");
 		network.weatherData(lat, lng); 
 		
 	});
-	
+		}else{
+			alert("Check network cennection!");
+		};
 	 }else {
-		alert("Please enable location settings on device!");
-	}
+		
+		if (Ti.Geolocation.locationServicesEnabled){
+		//Ti.Geolocation.purpose = ("Application needs your location cordinates");
+		Ti.Geolocation.getCurrentPosition(function(e){
+		console.log(e);
+			var lat = 0;
+			console.log(lat);
+			var lng = 0;
+            
+        console.log("theses are cordinates", lat, lng);
+        var network = require("network");
+		network.weatherData(lat, lng);
+	});	
+		 
+			}else{
+				alert("Check network cennection!");
+		};
+	};
 };
-
 exports.runGetGeo = getGeo;
